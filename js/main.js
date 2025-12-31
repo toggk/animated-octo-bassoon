f42539409e841a99b57d91cb9a7f649cd4bdbdc8
jkip <peterwu91695@gmail.com>
2025-12-30 23:17:40 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ const canvas = document.getElementById('canvas');
 
         class Particle {
             constructor(x, y, type) {
+                this.mass = 1;
                 this.x = x;
                 this.y = y;
                 this.speed = 1 + Math.random() * 2;
@@ -33,10 +34,10 @@ const canvas = document.getElementById('canvas');
             }
 
             update() {
-                    
+                
                 if this.collision_speed {
                    if this.collision_speed == 0 {this.collision_angle = 0}
-                   else {this.collision_speed -= 3}
+                   else {this.collision_speed -= this.mass}
                 }
                 
                 if (this.hasCollided) {
